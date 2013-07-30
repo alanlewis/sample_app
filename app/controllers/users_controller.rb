b7f11ded1b85e45ce6f5c31a43cea089d77dc533
Alan Lewis <alanlewis@gmail.com>
2013-07-30 08:49:49 -0700

    Through section 9.1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,31 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-  	def new
-  		@user = User.new
-  	end
+  def new
+		@user = User.new
+  end
 
-  	def create
-  		@user = User.new(params[:user])
-  		if @user.save
-        sign_in @user
-  			flash[:success] = "Welcome to the Sample App!"
-  			redirect_to @user
-  		else
-  			render 'new'
-  		end
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+      sign_in @user
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+		else
+    	render 'new'
   	end
+	end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(:params[:id])
+    if @user.update_attributes(param[:user])
+      # Handle a successful update
+    else
+      render 'edit'
+    end
+  end
 end
